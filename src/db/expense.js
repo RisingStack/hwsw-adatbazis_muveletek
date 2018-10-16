d0caf6d3330474bc6fe6410f139d844e4839b5e6
Mate Huszarik <matehuszarik@gmail.com>
2018-10-16 20:01:51 +0200

    expenses

--- src/db/expense.js
+++ src/db/expense.js
@@ -1,11 +1,41 @@
+const { ObjectId } = require('mongodb');
 const { getDB } = require('./index');
 
+const collectionName = 'expenses';
+
 async function list() {
-  const result = await getDB().collection('expenses').find().toArray();
+  const result = await getDB().collection(collectionName).find().toArray();
+  // .skip().limit().sort() ...
 
   return result;
 }
 
+async function get(id) {
+  const result = await getDB().collection(collectionName).findOne({ _id: ObjectId(id) });
+
+  return result;
+}
+
+async function insert(data) {
+  await getDB().collection(collectionName).insertOne(data);
+
+  return data;
+}
+
+async function update(id, data) {
+  await getDB().collection(collectionName).updateOne({ _id: ObjectId(id) }, { $set: data });
+
+  return data;
+}
+
+async function remove(id) {
+  await getDB().collection(collectionName).deleteOne({ _id: ObjectId(id) });
+}
+
 module.exports = {
   list,
+  get,
+  insert,
+  update,
+  remove,
 };
--- src/web/expenseHandler.js
+++ src/web/expenseHandler.js
@@ -1,7 +1,23 @@
+const joi = require('joi');
 const expenses = require('../db/expense');
+// const logger = require('../logger');
+
+const expenseSchema = joi.object({
+  name: joi.string().required(),
+  amount: joi.number().min(1).required(),
+}).unknown().required();
 
 async function get(req, res) {
+  const expenseID = req.params.id;
 
+  const result = await expenses.get(expenseID);
+
+  if (!result) {
+    res.status(404);
+    res.end();
+  }
+
+  res.send(result);
 }
 
 async function list(req, res) {
@@ -11,15 +27,41 @@ async function list(req, res) {
 }
 
 async function insert(req, res) {
+  try {
+    joi.attempt(req.body, expenseSchema);
+  } catch (err) {
+    res.status(400);
+    res.send(err.details[0].message);
+  }
 
+  const result = await expenses.insert(req.body);
+
+  res.status(201);
+  res.send(result);
 }
 
 async function update(req, res) {
+  try {
+    joi.attempt(req.body, expenseSchema);
+  } catch (err) {
+    res.status(400);
+    res.send(err.details[0].message);
+  }
 
+  const expenseID = req.params.id;
+
+  const result = await expenses.update(expenseID, req.body);
+
+  res.send(result);
 }
 
 async function remove(req, res) {
+  const expenseID = req.params.id;
 
+  await expenses.remove(expenseID);
+
+  res.status(204);
+  res.end();
 }
 
 module.exports = {
